6e03c5997710ae506e5f518719fd88ca12e16b34
moniquesenjaya <monique.senjaya@binus.ac.id>
2020-10-26 20:47:52 +0700

    Created Assignment 1

---
+++ Assignment 1/main.py
@@ -0,0 +1,18 @@
+inventory = {
+    'gold' : 500,
+    'pouch' : ['flint', 'twine', 'gemstone'],
+    'backpack' : ['xylophone','dagger', 'bedroll','bread loaf']
+}
+
+#Adding pocket as a key and values of it
+inventory['pocket'] = ['seashell', 'strange berry', 'lint']
+
+#Sorting the values under backpack key
+inventory['backpack'].sort()
+
+#Remove dagger from the backpack key
+inventory['backpack'].remove('dagger')
+
+#Adding 50 under the gold key
+inventory['gold'] = [500, 50]
+
