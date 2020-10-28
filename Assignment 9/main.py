f709da031812ae79f093caed76e9fdcbcfd68c41
moniquesenjaya <monique.senjaya@binus.ac.id>
2020-10-28 11:15:17 +0700

    Edited code to make it more efficient

--- Assignment 9/main.py
+++ Assignment 9/main.py
@@ -1,4 +1,5 @@
 f = open("file.txt", "r", encoding = 'utf-8')
+titles = ["Mr.", "Ms.", "Dr.", "Mrs.", "Jr."]
 words = f.read().split()
 counter = -1
 for word in words:
@@ -7,12 +8,10 @@ for word in words:
         if counter+1 < len(words):
             words[counter+1] = "\n" + words[counter+1]
     if word[-1] == ".":
-        if word == "Mr." or word == "Mrs." or word == "Dr." or word == "Jr." or word == "Ms.":
+        if word in titles:
             pass
         elif counter+1 < len(words):
             if words[counter+1][0].isupper():
                 words[counter+1] = "\n" + words[counter+1]
-        else:
-            pass
 print(" ".join(words))
 
